ad67504e33db5dcf6c2a9daacf4c6ef8f297fb62
wojtbauer <wojciech.brk1@gmail.com>
2018-09-10 12:12:50 +0200

    Problem 5 solved

--- Problem_5.py
+++ Problem_5.py
@@ -3,3 +3,17 @@
 # getString: to get a string from console input
 # printString: to print the string in upper case.
 # Also please include simple test function to test the class methods.
+
+class InputOutString(object):
+    def __init__(self):
+        self.s = ""
+
+    def getString(self):
+        self.s = input("Wpisz coś: ")
+
+    def printString(self):
+        print (self.s.upper())
+
+Obj = InputOutString()
+Obj.getString()
+Obj.printString()
