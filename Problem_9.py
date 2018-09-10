2218f22950a5f431c1f2e98d90418bba37c9be82
wojtbauer <wojciech.brk1@gmail.com>
2018-09-10 21:52:55 +0200

    Problem 9

---
+++ Problem_9.py
@@ -0,0 +1,14 @@
+# Question 9
+# Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence
+# capitalized.
+# Suppose the following input is supplied to the program:
+# Hello world
+# Practice makes perfect
+# Then, the output should be:
+# HELLO WORLD
+# PRACTICE MAKES PERFECT
+
+tekst = str(input("Tu wpisz tekst: "))
+tekst1 = str(input("Tu wpisz tekst: "))
+print(tekst.upper())
+print(tekst1.upper())
