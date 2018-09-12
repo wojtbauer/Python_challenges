627c06c4caeb81fcff7a2b874010625ef222475e
wojtbauer <wojciech.brk1@gmail.com>
2018-09-12 21:28:42 +0200

    Problem 11

---
+++ Problem_11.py
@@ -0,0 +1,20 @@
+# Question 11
+#
+# Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether
+# they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+# Example:
+# 0100,0011,1010,1001
+# Then the output should be:
+# 1010
+# Notes: Assume the data is input by console.
+
+inTab = [x for x in input("Wpisz liczby na listę: ").split(',')]
+outTab = []
+
+for x in inTab:
+    p = int(x, 2)
+    if p % 5 == 0:
+        outTab.append(x)
+
+sign = ','
+print(sign.join(outTab))
